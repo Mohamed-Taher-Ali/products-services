eb7f9fd33659684efcb0558cd00384bbf13dbf2f
Mohamed Taher <moslem.dev.2016@gmail.com>
2025-02-16 23:51:54 +0200

    feat: add products-provider types

---
+++ products-provider/src/types/product.d.ts
@@ -0,0 +1,35 @@
+type ProductVersion = 'V1' | 'V2';
+
+type Price = { value: number; date: Date };
+
+type BaseProduct = {
+  id: number;
+  price: Price;
+  currency: string;
+  lastUpdated: Date;
+  availability: boolean;
+};
+
+export type ProductVariation<Version extends ProductVersion, Body extends {}> = BaseProduct &
+  Body & {
+    version: Version;
+  };
+
+export type IProductV1 = ProductVariation<
+  'V1',
+  {
+    description: string;
+    name: string;
+  }
+>;
+
+export type IProducts = (IProductV1 | IProductV2)[];
+export type IProduct = Omit<IProductV1, 'version'>;
+
+export type IProductV2 = ProductVariation<
+  'V2',
+  {
+    productName: string;
+    desc: string;
+  }
+>;
